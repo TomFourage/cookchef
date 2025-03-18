72d2f6c0bb898df85ecbb24984030c7435af28f5
Tom <tom.fourage.dev@gmail.com>
2025-03-18 16:34:49 +0100

    partie 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
 import styles from "./Header.module.scss";
 import logo from "../assets/images/logo.svg";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 //<i class="fa-solid fa-bars"></i>
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars"></i>
       <div className="flex-fill">
         <img src={logo} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i className="fa-solid fa-basket-shopping mr-5"></i>
-          <span>Panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`${styles.headerXs} fa-solid fa-bars`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
 function Recipe({title, image}) {
+  const [liked, setLiked] = useState(false);
+  function handleClick() {
+    setLiked(!liked);
+  }
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
         <img src={image} alt="recette" />
       </div>
       <div
-        className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
-        <h3>{title}</h3>
+        <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ""}`}></i>
       </div>
     </div>
   );
